0ca06ac0e63b3873a6d3ee756728ee995b0e04d7
bmsmostafiz <bms.mostafiz@gmail.com>
2023-06-24 22:45:05 +0600

    added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 //--------- Script for nav Menue
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -21,12 +19,41 @@ const readMore = document.querySelector("#read-more");
 const experience = document.querySelector("#experience");
 const secondPara = document.querySelector("#second_para");
 
-readMore.addEventListener("click", () =>{
+readMore.addEventListener("click", () => {
     experience.classList.toggle("show");
     secondPara.classList.toggle("show");
-    if(readMore.innerHTML === "Read More"){
+    if (readMore.innerHTML === "Read More") {
         readMore.innerHTML = "Read Less";
-    }else{
+    } else {
         readMore.innerHTML = "Read More";
     }
 })
+
+//------------------- Testimonials area
+
+var testimonials = document.getElementById('testimonials');
+var control1 = document.getElementById('control1');
+var control2 = document.getElementById('control2');
+var control3 = document.getElementById('control3');
+
+
+control1.onclick = function () {
+    testimonials.style.transform = "translateX(870px)";
+    control1.classList.add("active");
+    control2.classList.remove("active");
+    control3.classList.remove("active");
+}
+
+control2.onclick = function () {
+    testimonials.style.transform = "translateX(0px)";
+    control1.classList.remove("active");
+    control2.classList.add("active");
+    control3.classList.remove("active");
+}
+
+control3.onclick = function () {
+    testimonials.style.transform = "translateX(-870px)";
+    control1.classList.remove("active");
+    control2.classList.remove("active");
+    control3.classList.add("active");
+}
