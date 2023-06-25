fdc9eb56266fca54371165a33bc80ae3feded85d
bmsmostafiz <bms.mostafiz@gmail.com>
2023-06-25 23:05:03 +0600

    added

--- js/main.js
+++ js/main.js
@@ -31,29 +31,80 @@ readMore.addEventListener("click", () => {
 
 //------------------- Testimonials area
 
-var testimonials = document.getElementById('testimonials');
-var control1 = document.getElementById('control1');
-var control2 = document.getElementById('control2');
-var control3 = document.getElementById('control3');
+// var testmonial = document.getElementById('testmonial');
+// var control1 = document.getElementById('control1');
+// var control2 = document.getElementById('control2');
+// var control3 = document.getElementById('control3');
+// var control4 = document.getElementById('control4');
 
 
-control1.onclick = function () {
-    testimonials.style.transform = "translateX(870px)";
+// control1.onclick = function () {
+//     testmonial.style.transform = "translateX(0px)";
+//     control1.classList.add("active");
+//     control2.classList.remove("active");
+//     control3.classList.remove("active");
+//     control4.classList.remove("active");
+// }
+
+// control2.onclick = function () {
+//     testmonial.style.transform = "translateX(-900px)";
+//     control1.classList.remove("active");
+//     control2.classList.add("active");
+//     control3.classList.remove("active");
+//     control4.classList.remove("active");
+// }
+
+// control3.onclick = function () {
+//     testmonial.style.transform = "translateX(-1800px)";
+//     control1.classList.remove("active");
+//     control2.classList.remove("active");
+//     control3.classList.add("active");
+//     control4.classList.remove("active");
+// }
+
+// control4.onclick = function () {
+//     testmonial.style.transform = "translateX(-2700px)";
+//     control1.classList.remove("active");
+//     control2.classList.remove("active");
+//     control3.classList.remove("active");
+//     control4.classList.add("active");
+// }
+
+const testmonial = document.querySelector("#testmonial");
+const control1 = document.querySelector("#control1");
+const control2 = document.querySelector("#control2");
+const control3 = document.querySelector("#control3");
+const control4 = document.querySelector("#control4");
+
+
+control1.addEventListener("click", () => {
+    testmonial.style.transform = "translateX(0px)";
     control1.classList.add("active");
     control2.classList.remove("active");
     control3.classList.remove("active");
-}
+    control4.classList.remove("active");
+})
 
-control2.onclick = function () {
-    testimonials.style.transform = "translateX(0px)";
+control2.addEventListener("click", () => {
+    testmonial.style.transform = "translateX(-900px)";
     control1.classList.remove("active");
     control2.classList.add("active");
     control3.classList.remove("active");
-}
+    control4.classList.remove("active");
+})
 
-control3.onclick = function () {
-    testimonials.style.transform = "translateX(-870px)";
+control3.addEventListener("click", () => {
+    testmonial.style.transform = "translateX(-1800px)";
     control1.classList.remove("active");
     control2.classList.remove("active");
     control3.classList.add("active");
-}
+    control4.classList.remove("active");
+})
+
+control4.addEventListener("click", () => {
+    testmonial.style.transform = "translateX(-2700px)";
+    control1.classList.remove("active");
+    control2.classList.remove("active");
+    control3.classList.remove("active");
+    control4.classList.add("active");
+})
